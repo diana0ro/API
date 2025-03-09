27f4a66944cbd7ea8d9d50bc2dbb8dd49b574174
diana0ro <72491435+diana0ro@users.noreply.github.com>
2025-03-09 13:46:59 +0200

    test1

---
+++ test_attempt.py
@@ -0,0 +1,8 @@
+import requests
+
+def test_get_users():
+    # Send a GET request to the API
+    response = requests.get("http://localhost/#/store/getInventory")
+        
+    # Assert the response status code is 200 (OK)
+    assert response.status_code == 200
